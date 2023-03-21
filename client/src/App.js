76b95e6a93c6dfb151615a8dcaf51c68adfe09b8
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-03-20 22:02:58 -0300

    create components Post and Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,12 @@
 import './App.css';
+import Post from './components/Post';
+import Header from './components/Header';
 
 function App() {
   return (
     <main>
-        <header>
-            <a href="/" className="logo">Meu Blog</a>
-            <nav className="">
-                <a href="/">Login</a>
-                <a href="/">Register</a>
-            </nav>
-        </header>
-        <div className="post">
-            <div className="image">
-            <img src="https://images.unsplash.com/photo-1679215805552-e28a96550a23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" className="" />
-            </div>
-            <div className="texts">
-                <h2 className="">Design moderno para sua casa</h2>
-                <p className="info">
-                    <a className="author">Eduardo Melo</a>
-                    <time>20/03/2023 21:37</time>
-                </p>
-                <p className="summary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna 
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                </p>   
-            </div>
-        </div>
-        <div className="post">
-            <div className="image">
-            <img src="https://images.unsplash.com/photo-1679215805552-e28a96550a23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" className="" />
-            </div>
-            <div className="texts">
-                <h2 className="">Design moderno para sua casa</h2>
-                <p className="info">
-                    <a className="author">Eduardo Melo</a>
-                    <time>20/03/2023 21:37</time>
-                </p>
-                <p className="summary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna 
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                </p>  
-            </div>
-        </div>
-        <div className="post">
-            <div className="image">
-            <img src="https://images.unsplash.com/photo-1679215805552-e28a96550a23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" className="" />
-            </div>
-            <div className="texts">
-                <h2 className="">Design moderno para sua casa</h2>
-                <p className="info">
-                    <a className="author">Eduardo Melo</a>
-                    <time>20/03/2023 21:37</time>
-                </p>
-                <p className="summary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna 
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                </p>   
-            </div>
-        </div>
+        <Header />
+        <Post />
     </main>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+            <a href="/" className="logo">Meu Blog</a>
+            <nav className="">
+                <a href="/">Login</a>
+                <a href="/">Register</a>
+            </nav>
+        </header>
+    )
+}
