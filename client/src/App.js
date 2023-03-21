67e791ab6f43215e39cb137b6f8dc0be7955a69e
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-03-20 21:21:15 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+        <header>
+            <a href="/" className="logo">Meu Blog</a>
+            <nav className="">
+                <a href="/">Login</a>
+                <a href="/">Register</a>
+            </nav>
+        </header>
+        <div className="post">
+            <div className="image">
+            <img src="https://images.unsplash.com/photo-1679215805552-e28a96550a23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" className="" />
+            </div>
+            <div className="texts">
+                <h2 className="">Design moderno para sua casa</h2>
+                <p className="">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu f
+                </p>   
+            </div>
+        </div>
+        <div className="post">
+            <div className="image">
+            <img src="https://images.unsplash.com/photo-1679215805552-e28a96550a23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" className="" />
+            </div>
+            <div className="texts">
+                <h2 className="">Design moderno para sua casa</h2>
+                <p className="">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu f
+                </p>   
+            </div>
+        </div>
+        <div className="post">
+            <div className="image">
+            <img src="https://images.unsplash.com/photo-1679215805552-e28a96550a23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" className="" />
+            </div>
+            <div className="texts">
+                <h2 className="">Design moderno para sua casa</h2>
+                <p className="">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fsdsd
+                </p>   
+            </div>
+        </div>
+    </main>
+  );
+}
+
+export default App;
