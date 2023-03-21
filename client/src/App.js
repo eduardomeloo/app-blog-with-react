c3c95be04f81f1158f93d11a07aee9422eb15ede
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-03-21 19:51:25 -0300

    create component Layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import Post from './components/Post';
 import Header from './components/Header';
 import {Route, Routes} from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-        <Route index element={
-            <main>
-                <Header />
-                <Post />
-            </main>
-        } />
+        <Route path="/" element={<Layout />}>
+
+            <Route index element={<Post />} />
+
+            <Route path="/login" element={
+                <div className="">login</div>
+            } />
+        </Route>
     </Routes>
   );
 }
