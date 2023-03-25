3af3252f143ace984a531ab6196ca3aad5ed4256
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-03-25 08:15:22 -0300

    initial configuration to send form to API

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
-
+//Documentação: https://yarnpkg.com/package/react-quill#readme
 const modules = {
     toolbar: [
         [{ 'header': [1, 2, false] }],
@@ -20,19 +20,40 @@ const formats = [
 ]
 
 export default function CreatePost() {
-    const [title, setTitle] = useState('');
+    const [title, setTitle]     = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
+    const [files, setFiles]       = useState('');
+
+    function createNewPost(ev) {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        //data.set('file', file);
+
+        ev.preventDefault();
+        
+        console.log(files)
+        
+        // fetch('http://localhost:4001/post', {
+        //     method: 'POST',
+        //     credentials: 'include',
+        //     body: 
+        // })
+    
+    }
 
     return (
-        <form>
+        <form onSubmit={createNewPost} >
             <input type="title"   placeholder="Title" 
                 value={title} onChange={ev => setTitle(ev.target.value)} />
 
             <input type="summary" placeholder="Summary" 
                 value={summary} onChange={ev => setSummary(ev.target.value)} />
 
-            <input type="file" />
+            <input type="file" value={files} 
+                onChange={ev => setFiles(ev.target.files)} />
 
             <ReactQuill value={content} modules={modules} formats={formats}
                 onChange={newValue => setContent(newValue)} />
