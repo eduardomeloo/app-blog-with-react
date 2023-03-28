0b60bbb2c632c70a1e91cb5957947c4829bd7e49
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-03-27 21:19:41 -0300

    ajuste na responsividade

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {formatISO9075} from 'date-fns';
 
 export default function PostPage() {
     const [postInfo, setPostInfo] = useState(null);
@@ -17,12 +18,14 @@ export default function PostPage() {
 
     return (
         <div className="post-page">
+            <h1>{postInfo.title}</h1>
+            <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+            <div className="author">by {postInfo.author.username}</div>
             <div>
                 <div className="image">
                     <img src={`http://localhost:4001/${postInfo.cover}`} alt="" />
                 </div>
             </div>
-            <h1>{postInfo.title}</h1>
             <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
         </div>
     );
